c9edafee18a72ea39c8c5e3c6aba62a555f1b580
Peklerfix <peklerfix@gmail.com>
2024-01-31 21:35:40 +0100

    CORS works

--- src/api/cartService.ts
+++ src/api/cartService.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import { AboutCart } from '../types/cartTypes';
 
+axios.defaults.withCredentials = true;
+
 export const getCartContent = async () => {
   const response = await axios.get<AboutCart>('https://localhost:5445/api/Carts', {
-    withCredentials: true
   });
   return response.data;
 };
--- src/api/productService.ts
+++ src/api/productService.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { ProductDetails } from '../types/productTypes';
 
+axios.defaults.withCredentials = true;
+
 export const getAllProducts = async () => {
   const response = await axios.get<ProductDetails[]>('https://localhost:5445/api/Products');
   return response.data;
