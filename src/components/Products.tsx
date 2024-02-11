3d6221c4f8524d4e12cf5fe9c8d81d6e98a8260b
Ksztof <krzysztofkozlowski1995@gmail.com>
2024-02-11 14:26:53 +0100

    Products com,eponet update and have added new type

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -3,13 +3,12 @@ import { useAppDispatch } from "../hooks";
 import { getProducts } from "../redux/actions/productActions";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
-import { ProductDetails } from "../types/productTypes";
+import { ProductDetails, ProductInCart } from "../types/productTypes";
 
 export const Products = () => {
     const dispatch = useAppDispatch();
     const isDataLoading = useSelector((state: RootState) => state.product.loading);
     const products = useSelector((state: RootState) => state.product.productsData);
-
 
     useEffect(() => {
         if(!isDataLoading){
@@ -17,6 +16,17 @@ export const Products = () => {
         }
     }, []);
     
+    const addToCart = (product: ProductInCart, quantity: number) => {
+        const itemsInCart = JSON.parse(localStorage.getItem('cart') || '[]');
+
+        const existingItem: ProductInCart = itemsInCart.find((item: ProductInCart) => item.id === product.id);
+        if(existingItem){
+            existingItem.quantity += 1
+            
+        }
+
+    }
+
     return (
         <div>
             {products.map((product: ProductDetails) => (
@@ -24,7 +34,8 @@ export const Products = () => {
                     <h3>Name: {product.name}</h3>
                     <p>Description: {product.description}</p>
                     <p>price: {product.price}</p>
-                    <button >+</button>
+                    <input></input>
+                    <button >dodaj do koszyka</button>
                 </div>
             ))}
         </div>
