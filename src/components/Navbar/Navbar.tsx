720b8aa79b424dac2e5d9a494847d6f7fda891cb
krzysztof kozlowski <krzysztofkozlowski1995@gmail.com>
2024-06-28 01:37:52 +0100

    navbar hamburger menu has been added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,17 @@
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { RootState, persistor } from '../../redux/store';
+import { RootState } from '../../redux/store';
 import { useAppDispatch } from '../../hooks';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { resetAuth } from '../../redux/actions/authActions';
 import styles from './Navbar.module.scss'
 import logo from '../../pictures/hpcLogo.png';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 export const Navbar: React.FC = () => {
-    const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
     const dispatch = useAppDispatch();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
 
     useEffect(() => {
         console.log(isLoggedIn);
@@ -19,18 +21,27 @@ export const Navbar: React.FC = () => {
         dispatch(resetAuth());
     };
 
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <div className={styles.navbar}>
             <div className={styles.logo}>
-                <img className={styles.img}src={logo} alt="hpcLogo" loading="lazy" />
+                <img className={styles.img} src={logo} alt="hpcLogo" loading="lazy" />
             </div>
-            <div className={styles.navbarOptions}>
+            <div className={`${styles.navbarOptions} ${isMenuOpen ? styles.open : ""}`}>
                 <nav className={styles.nav}>
                     <Link className={styles.navbarLink} to="/"> Main </Link>
                     {isLoggedIn ? <Link className={styles.navbarLink} to="" onClick={handleLogout}>Logout</Link> : <Link className={styles.navbarLink} to="/login"> Login </Link>}
                     <Link className={styles.navbarLink} to="/register"> Register </Link>
                 </nav>
             </div>
+            <div className={styles.hamburger} onClick={toggleMenu}>
+                {isMenuOpen ? <FaTimes /> : <FaBars />}
+            </div>
         </div>
     );
 };
+
+export default Navbar;
