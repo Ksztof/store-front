6632a0453b967d1920895e94ca250c9c7a5a9c2f
krzysztof kozlowski <krzysztofkozlowski1995@gmail.com>
2024-06-30 13:58:40 +0100

    filters and cart content view is opening onclick

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -5,38 +5,40 @@ import styles from './MainPage.module.scss';
 import { FaChevronLeft, FaChevronRight, FaShoppingCart } from 'react-icons/fa';
 
 export const Main = () => {
-    const [isCartOpen, setIsCartOpen] = useState(true);
+    const [isCartOpen, setIsCartOpen] = useState(false);
+    const [isFiltersOpen, setIsFiltersOpen] = useState(false);
 
     const toggleCart = () => {
         setIsCartOpen(!isCartOpen);
     };
 
+    const toggleFilters = () => {
+        setIsFiltersOpen(!isFiltersOpen);
+    };
+
     return (
         <div className={styles.mainContainer}>
-            <div className={styles.optionsCartSection}>
+            <div className={styles.optionsCartSection} onClick={toggleCart}>
                 <div className={styles.cartLogo}>
                     <FaShoppingCart size={15} />
                 </div>
             </div>
-            <div className={styles.optionsFiltersSection}>
+            <div className={styles.optionsFiltersSection} onClick={toggleFilters}>
                 <p>F</p>
             </div>
-
-            <div className={styles.cartFiltersContainer}>
-                <div className={styles.cartContent}>
+            <div className={`${styles.cartFiltersContainer} ${(isCartOpen || isFiltersOpen) ? styles.open : styles.closed}`}>
+                <div className={`${styles.cartContent} ${!isCartOpen ? styles.hidden : ''}`}>
                     <Cart />
                 </div>
-                <div className={styles.filtersContent}>
-                    <p>fitlers</p>
+                <div className={`${styles.filtersContent} ${!isFiltersOpen ? styles.hidden : ''}`}>
+                    <p>filters</p>
                 </div>
             </div>
-
-            <div className={`${styles.productsContainer} ${isCartOpen ? styles.cartOpen : styles.cartCollapsed}`}>
+            <div className={`${styles.productsContainer} ${(isCartOpen || isFiltersOpen) ? styles.cartOpen : styles.cartCollapsed}`}>
                 <Products />
             </div>
         </div>
     );
-}
+};
 
-            /* <div className={styles.optionsBar}> */
-            /* </div> */
+export default Main;
