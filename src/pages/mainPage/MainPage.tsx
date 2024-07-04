a52ca8e4c896c0161b8b09798c82ad22c5e7ab34
krzysztof kozlowski <krzysztofkozlowski1995@gmail.com>
2024-07-04 16:18:29 +0200

    correct margins for products container while filters/cart sections is closed and opened

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,41 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Products } from "../../components/Products";
 import styles from './MainPage.module.scss';
-import { FaFilter, FaShoppingCart, FaSlidersH, FaTimes } from 'react-icons/fa';
+import { FaShoppingCart, FaSlidersH, FaTimes } from 'react-icons/fa';
 import { Cart } from '../../components/cart/Cart';
 
 export const Main = () => {
     const [isCartOpen, setIsCartOpen] = useState<boolean>(false);
     const [isFiltersOpen, setIsFiltersOpen] = useState<boolean>(false);
-    const [cartAndFilterOpen, setCartAndFilterOpen] = useState<boolean>(false);
-
-    useEffect(() => {
-        const areBothOpen: boolean = isCartOpen && isFiltersOpen ? true : false;
-        setCartAndFilterOpen(areBothOpen);
-    }, [isCartOpen, isFiltersOpen]);
 
     const toggleCart = () => {
-        if (isFiltersOpen) {
+        if (isFiltersOpen)
             setIsFiltersOpen(false);
-        }
-
         setIsCartOpen(!isCartOpen);
     };
 
     const toggleFilters = () => {
-        if (isCartOpen) {
+        if (isCartOpen)
             setIsCartOpen(false);
-        }
-
         setIsFiltersOpen(!isFiltersOpen);
     };
 
     const closeOption = () => {
-        if (isCartOpen) {
+        if (isCartOpen)
             setIsCartOpen(false);
-        } else if (isFiltersOpen) {
+        else
             setIsFiltersOpen(false);
-        }
     };
 
     return (
@@ -43,11 +32,11 @@ export const Main = () => {
             <div className={styles.optionsBar}>
                 <div className={`${styles.showCartButton} ${isCartOpen ? styles.optionBarOpen : ''}`} onClick={toggleCart}>
                     <FaShoppingCart className={styles.cartLogo} />
-                    {isCartOpen && <FaTimes className={styles.closeCartLogo} />}
+                    {isCartOpen && <FaTimes className={styles.closeLogo} />}
                 </div>
                 <div className={`${styles.showFiltersButton} ${isFiltersOpen ? styles.optionBarOpen : ''}`} onClick={toggleFilters}>
                     <FaSlidersH className={styles.filtersLogo} />
-                    {isFiltersOpen && <FaTimes className={styles.closeFiltersLogo} />}
+                    {isFiltersOpen && <FaTimes className={styles.closeLogo} />}
                 </div>
             </div>
             <div className={
@@ -57,16 +46,15 @@ export const Main = () => {
                 ${isFiltersOpen ? styles.filtersContentOpen : ''}
                 `
             }>
-                <button className={styles.closeButton} onClick={closeOption}><FaTimes className={styles.closeButtonLogo} /></button>
+                <button className={styles.closeButton} onClick={closeOption}><FaTimes className={`${styles.closeButtonLogo} ${isCartOpen || isFiltersOpen ? styles.optionOpen : ''}`} /></button>
                 <div className={styles.cartContent}>
-
                     <Cart />
                 </div>
                 <div className={styles.filtersContent}>
                     <p>filters</p>
                 </div>
             </div>
-            <div className={styles.productsContainer}>
+            <div className={`${styles.productsContainer} ${isCartOpen || isFiltersOpen ? styles.leftBarOpen : ''}`}>
                 <Products />
             </div>
         </div>
