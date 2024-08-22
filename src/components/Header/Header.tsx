0e42cc43a5f39dc9777d2bb149b9730bf5976a43
krzysztof kozlowski <krzysztofkozlowski1995@gmail.com>
2024-08-22 15:44:28 +0200

    dwa

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import Navbar from "../Navbar/Navbar";
+import styles from "./Header.module.scss";
+
+export const Header: React.FC = () => {
+    return (
+        <header className={styles.header}>
+            <Navbar />
+        </header>
+    )
+}
